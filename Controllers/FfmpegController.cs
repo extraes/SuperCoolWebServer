fb673fa182a779e3d5422ab8a3066b19d9517de9
extraes <52384576+extraes@users.noreply.github.com>
2023-12-15 04:48:41 -0800

    add ffprobe api

---
+++ Controllers/FfmpegController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO.Compression;
+using System.Reflection.Emit;
+using Xabe.FFmpeg;
+
+namespace SuperCoolWebServer.Controllers;
+
+[Route("api/ffmpeg/[action]")]
+public class FfmpegController : Controller
+{
+    [HttpPut]
+    [ActionName("probe")]
+    [Consumes("video/mp4", "video/webm")]
+    public async Task<IActionResult> Probe([FromBody] Stream videoFile)
+    {
+        if (videoFile is null)
+            return BadRequest();
+
+        string filePath = Path.GetTempFileName();
+        using (FileStream fs = System.IO.File.OpenWrite(filePath))
+        {
+            await videoFile.CopyToAsync(fs);
+        }
+
+        IMediaInfo info;
+        try
+        {
+            info = await FFmpeg.GetMediaInfo(filePath);
+        }
+        catch(Exception ex)
+        {
+            return UnprocessableEntity(ex);
+        }
+
+        return Ok(new PathlessMediaInfo(info));
+    }
+
+}
---
+++ PathlessMediaInfo.cs
@@ -0,0 +1,49 @@
+﻿using Xabe.FFmpeg;
+
+namespace SuperCoolWebServer;
+
+internal class PathlessMediaInfo : IMediaInfo
+{
+    internal class PathlessStream : IStream
+    {
+        internal PathlessStream(IStream originalStream) => this.originalStream = originalStream;
+        readonly IStream originalStream;
+
+        public string Path => "";
+
+        public int Index => originalStream.Index;
+
+        public string Codec => originalStream.Codec;
+
+        public StreamType StreamType => originalStream.StreamType;
+
+        public string BuildParameters(ParameterPosition forPosition)
+        {
+            return originalStream.BuildParameters(forPosition);
+        }
+
+        public IEnumerable<string> GetSource()
+        {
+            return originalStream.GetSource();
+        }
+    }
+
+    internal PathlessMediaInfo(IMediaInfo originalInfo) => this.originalInfo = originalInfo;
+    readonly IMediaInfo originalInfo;
+
+    public IEnumerable<IStream> Streams => originalInfo.Streams.Select(s => new PathlessStream(s));
+
+    public string Path => "";
+
+    public TimeSpan Duration => originalInfo.Duration;
+
+    public DateTime? CreationTime => originalInfo.CreationTime;
+
+    public long Size => originalInfo.Size;
+
+    public IEnumerable<IVideoStream> VideoStreams => originalInfo.VideoStreams;
+
+    public IEnumerable<IAudioStream> AudioStreams => originalInfo.AudioStreams;
+
+    public IEnumerable<ISubtitleStream> SubtitleStreams => originalInfo.SubtitleStreams;
+}
