21e9c8229fe4ee361a35862e219fdf9c0e7fdd71
extraes <52384576+extraes@users.noreply.github.com>
2024-09-07 13:47:37 -0700

    add head tag & lang en

--- DiscordFormat.cs
+++ DiscordFormat.cs
@@ -3,13 +3,15 @@
 internal static class DiscordFormat
 {
     public const string LARGE_VIDEO_FORMAT = """
-        <html>
-            <meta property="og:image" content="{0}">
-            <meta property="og:type" content="video.other">
-            <meta property="og:video:url" content="{1}">
-            <meta property="og:video:secure_url" content="{1}">
-            <meta property="og:video:width" content="{2}">
-            <meta property="og:video:height" content="{3}">
+        <html lang="en">
+            <head>
+                <meta property="og:image" content="{0}">
+                <meta property="og:type" content="video.other">
+                <meta property="og:video:url" content="{1}">
+                <meta property="og:video:secure_url" content="{1}">
+                <meta property="og:video:width" content="{2}">
+                <meta property="og:video:height" content="{3}">
+            </head>
         </html>
         """;
 }
