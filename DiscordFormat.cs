c54b5b79dc46bc041c6832718304d017d9e3c4ca
extraes <52384576+extraes@users.noreply.github.com>
2024-09-07 14:23:15 -0700

    add actual html

--- DiscordFormat.cs
+++ DiscordFormat.cs
@@ -3,6 +3,7 @@
 internal static class DiscordFormat
 {
     public const string LARGE_VIDEO_FORMAT = """
+        <!DOCTYPE html>
         <html lang="en">
             <head>
                 <meta property="og:image" content="{0}">
@@ -12,6 +13,13 @@ internal static class DiscordFormat
                 <meta property="og:video:width" content="{2}">
                 <meta property="og:video:height" content="{3}">
             </head>
+            <body>
+              <p style="font-size: 1.2rem; font-family: sans-serif;">Copy this url into discord to see the embed<br>(Original video may be large)</p>
+              <pre style="background: #333; color: white; border-radius: 3px; overflow: auto;">
+                <code></code>
+              </pre>
+              <script>document.querySelector("code").textContent = document.head.innerHTML</script>
+            </body>
         </html>
         """;
 }
