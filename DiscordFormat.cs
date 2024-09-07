d48fc0d484176843280c03ea5d9259a442c31414
extraes <52384576+extraes@users.noreply.github.com>
2024-09-07 01:53:54 -0700

    add discord format constant

---
+++ DiscordFormat.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperCoolWebServer;
+
+internal static class DiscordFormat
+{
+    public const string LARGE_VIDEO_FORMAT = """
+        <meta property="og:image" content="{0}">
+        <meta property="og:type" content="video.other">
+        <meta property="og:video:url" content="{1}">
+        <meta property="og:video:width" content="{2}">
+        <meta property="og:video:height" content="{3}">
+        """;
+}
