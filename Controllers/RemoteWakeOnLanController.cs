e8b9a6c4b96b869ee5fb5b1562acfd7f09990b30
extraes <52384576+extraes@users.noreply.github.com>
2025-02-27 22:40:48 -0800

    try shelling out

--- Controllers/RemoteWakeOnLanController.cs
+++ Controllers/RemoteWakeOnLanController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace SuperCoolWebServer.Controllers
 {
@@ -7,15 +8,15 @@ namespace SuperCoolWebServer.Controllers
     {
         [HttpPut]
         [ActionName("get")]
-        public async Task<IActionResult> Wake(string auth, string mac)
+        public async Task<IActionResult> Wake(string auth, string mac, string ip = "255.255.255.255")
         {
             if (auth != Config.values.wolAuth)
             {
                 return Unauthorized();
             }
 
-            await WOL.WakeOnLan(mac);
-
+            Process.Start("wakeonlan", $"-i {ip} {mac}"); // TODO: use C# lib instead of shelling out
+            //await WOL.WakeOnLan(mac);
 
             return Ok();
         }
