60353a2feeae3a736e9d586ef71d2445960a2527
extraes <52384576+extraes@users.noreply.github.com>
2026-08-21 14:38:23 -0700

    Add audit log to DB

--- Migrations/20260821204449_AddAuditLog.Designer.cs
+++ Migrations/20260821204449_AddAuditLog.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuperCoolWebServer.Data;
 
@@ -10,9 +11,11 @@ using SuperCoolWebServer.Data;
 namespace SuperCoolWebServer.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260821204449_AddAuditLog")]
+    partial class AddAuditLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.11");
--- Models/AuditLogEntry.cs
+++ Models/AuditLogEntry.cs
@@ -1,8 +1,15 @@
-using SuperCoolWebServer.Models;
+using System.ComponentModel.DataAnnotations;
 
-namespace SuperCoolWebServer.Data;
+namespace SuperCoolWebServer.Models;
 
 public class AuditLogEntry : SnowflakeObject
 {
+    [MaxLength(64)] public required string Action { get; init; }
     
+    [MaxLength(128)] public string? EntityType { get; init; }
+    public long? EntityId { get; init; }
+    
+    public long? UserId { get; init; }
+    // 16KB is probably more than I need, but it's a good thing to have and not need than need and not have. 
+    [MaxLength(1024*16)] public string? DetailsJson { get; init; }
 }
