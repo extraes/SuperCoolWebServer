d45e9cd860eb2021bfe1487c18f6422fdf9baa3c
extraes <52384576+extraes@users.noreply.github.com>
2026-09-06 01:01:34 -0700

    document AuditLogWriter.cs

--- Data/AuditLogWriter.cs
+++ Data/AuditLogWriter.cs
@@ -2,6 +2,7 @@ using System.Security.Claims;
 using System.Text.Json;
 using Snowflakes;
 using SuperCoolWebServer.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace SuperCoolWebServer.Data;
 
@@ -9,6 +10,21 @@ public sealed class AuditLogWriter(
     DataContext db,
     SnowflakeGenerator<long> snowflakeGenerator)
 {
+    /// <summary>
+    /// Writes an entry into the DB's audit log table.
+    /// <para>If the caller (YOU) touches the DB, make sure you use <see cref="DatabaseFacade.BeginTransactionAsync"/></para>
+    /// <br/> (And make sure you call <see cref="DatabaseFacade.CommitTransactionAsync"/> once you're done with everything) 
+    /// </summary>
+    /// <param name="httpContext">The context from which an action was taken.</param>
+    /// <param name="actorUserId">The ID of the user taking the action. If it's null, the ID will be gathered from <br/>
+    ///                           the <paramref name="httpContext"/> instead.</param>
+    /// <param name="action">A string representing what was done.
+    ///                      Typically taken from <see cref="AuditLogStrings"/>.</param>
+    /// <param name="entityType">A string representing what was made/deleted/changed.
+    ///                          Typically taken from <see cref="AuditLogStrings"/>.</param>
+    /// <param name="entityId">The snowflake ID of the thing made/deleted/changed.</param>
+    /// <param name="details">An object (usually anonymous type) containing details of what happened.</param>
+    /// <param name="cancellationToken">A cancellation token used to cancel this operation.</param>
     public async Task WriteAsync(HttpContext httpContext,
         long? actorUserId,
         string action,
@@ -25,23 +41,21 @@ public sealed class AuditLogWriter(
             Data = details,
         });
         
-        if (jsonStr.Length > AuditLogEntry.DETAILS_MAX_LENGTH)
+        var originalJson = jsonStr;
+        int shortenBy = 128;
+            
+        while (jsonStr.Length > AuditLogEntry.DETAILS_MAX_LENGTH)
         {
-            var originalJson = jsonStr;
-            int shortenBy = 128;
-            while (jsonStr.Length > AuditLogEntry.DETAILS_MAX_LENGTH)
+            jsonStr = JsonSerializer.Serialize(new
             {
-                jsonStr = JsonSerializer.Serialize(new
+                RequestId = httpContext.TraceIdentifier,
+                Data = new
                 {
-                    RequestId = httpContext.TraceIdentifier,
-                    Data = new
-                    {
-                        TruncatedData = originalJson[..(AuditLogEntry.DETAILS_MAX_LENGTH - shortenBy)],
-                    }
-                }); 
-                
-                shortenBy += 128;
-            }
+                    TruncatedData = originalJson[..(AuditLogEntry.DETAILS_MAX_LENGTH - shortenBy)],
+                }
+            }); 
+            
+            shortenBy += 128;
         }
         
         var entry = new AuditLogEntry
